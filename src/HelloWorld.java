7f6d79006900de9686ea889b717580427c2cc7b2
Vivek Gupta <vivek.gupta@razorthink.net>
2016-11-16 18:47:53 +0530

    resolve conflicts

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,10 +4,7 @@
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World!");
-<<<<<<< HEAD
+
         System.out.println("Conflict");
-=======
-        System.out.println("Conflict!");
->>>>>>> ecbdb5927400615bfd784de62a3ccde2ad383857
     }
 }
